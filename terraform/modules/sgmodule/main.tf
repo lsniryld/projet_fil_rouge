68fe8fca36c875e1a93e7ab7ce75a18ed35ea847
lsniryld <adrianajade.erin@gmail.com>
2023-01-31 16:41:18 +0100

    add ansible and terraform

---
+++ terraform/modules/sgmodule/main.tf
@@ -0,0 +1,63 @@
+resource "aws_security_group" "allow_ssh_http_https" {
+  name        = var.sgname
+  description = "Allow http and https inbound traffic"
+
+  ingress {
+    description = "http from VPC"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "odoo port"
+    from_port   = 8069
+    to_port     = 8069
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "postgress port"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
+    description = "pgadmin port"
+    from_port   = 5050
+    to_port     = 5050
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  tags = {
+    Name = "${var.maintainer}-sg"
+  }
+}
---
+++ terraform/modules/sgmodule/variables.tf
@@ -0,0 +1,12 @@
+
+variable "sgname" {
+  type        = string
+  description = "set security group name"
+  default     = "niry-sg"
+}
+
+variable "maintainer" {
+  type        = string
+  description = "maintainer name"
+  default     = "niry"
+}
